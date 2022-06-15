aa0f1e1045b51279de0c9e03e5766bac008a3fcc
brijeshkuntal <brijesh.kuntal@nagarro.com>
2022-06-15 11:42:50 +0530

    code formatting

--- FrontEnd/src/app/layout/App.tsx
+++ FrontEnd/src/app/layout/App.tsx
@@ -1,12 +1,12 @@
 import { observer } from "mobx-react-lite";
 import WithoutOktaAuthPage from "./WithoutOktaAuthPage";
-import LoginConfigJSON from "./../../loginConfig.json";
+import AppConfigJSON from "./../../appConfig.json";
 import OktaAuthPage from "./OktaAuthPage";
 import { ToastContainer } from "react-toastify";
 import ModalContainer from "../common/modals/ModalContainer";
 
 function App() {
-  const { isOktaLoginEnabled } = LoginConfigJSON;
+  const { isOktaLoginEnabled } = AppConfigJSON;
   return (
     <>
       <ToastContainer position="bottom-right" hideProgressBar />
